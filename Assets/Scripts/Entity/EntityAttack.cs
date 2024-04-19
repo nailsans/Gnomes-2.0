c8eaf5f97b20eba882e5ba8ee3b8594436a014df
JegiorO <112984353+JegiorO@users.noreply.github.com>
2024-04-19 22:48:24 +0300

    Revert "VELIKIY MERGE! VSEM HAYPOVAT'"
    This reverts commit b6bd8580b24660ba661bceed1a46674efebe9504, reversing
    changes made to 38d12aa3e86af27ee066f7b080a34c18c8ece308.

--- Assets/Scripts/Entity/EntityAttack.cs
+++ Assets/Scripts/Entity/EntityAttack.cs
@@ -4,71 +4,27 @@ using UnityEngine;
 
 public class EntityAttack : MonoBehaviour
 {
-    // A script applied on entity that allows to implement attack for it
-    [SerializeField] bool attacksPlayers = true;
-    [SerializeField] bool attacksPeneks = false;
-    [SerializeField] private int damage = 3;
-    private GameObject model;
-    Animator animator;
-    private EntityMove _entityMove;
-
     [SerializeField] private GameObject attackArea = default(GameObject);
     private bool isAttacking = false;
-    private float timeToAttack = 0.65f;
+    private float timeToAttack = 0.25f;
 
-    private void Awake()
+    private void Start()
     {
-        animator = GetComponentInChildren<Animator>();
-        _entityMove = GetComponent<EntityMove>();
-        model = transform.Find("Model").gameObject;
+        isAttacking = false;
+        attackArea.SetActive(isAttacking);
     }
 
-    public void startAttacking(Collider other)
-    {
-        if (!attacksPlayers) return;
-        PlayerHPSystem playerHP = other.GetComponent<PlayerHPSystem>();
-        if (playerHP == null) return;
-        StartCoroutine(IAttackCycle(other));
-    }
-
-    public void StopAttacking()
-    {
-        _entityMove.targetIsClose = false;
-        StopAllCoroutines();
-    }
-
-    private IEnumerator IAttackCycle(Collider other)
-    {
-        //if (!attacksPlayers) yield break;
-        PlayerHPSystem playerHP = other.GetComponent<PlayerHPSystem>();
-        //if (playerHP == null) yield break;
-        while (true)
-        {
-            if (attacksPlayers)
-            {
-                if (playerHP != null)
-                {
-                    animator.SetTrigger("hit");
-                    _entityMove.targetIsClose = true;
-                    playerHP.takeDamage(damage);
-                    
-                }
-            }
-
-            yield return new WaitForSeconds(timeToAttack);
-        }
-    }
-
-    private void bettaAttackAnim()
+    public void Attack()
     {
         StartCoroutine(IAttack());
     }
 
     private IEnumerator IAttack()
     {
-        Vector3 startPos = model.transform.localPosition;
-        model.transform.localPosition = new Vector3(startPos.x, startPos.y + 1, startPos.z);
-        yield return new WaitForSeconds(0.3f);
-        model.transform.localPosition = startPos;
+        isAttacking = true;
+        attackArea.SetActive(isAttacking);
+        yield return new WaitForSeconds(timeToAttack);
+        isAttacking = false;
+        attackArea.SetActive(isAttacking);
     }
 }
